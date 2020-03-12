f6b956169280e3ab3e073abbfdb28de26d74eaad
Rajesh2545 <rajeshnalleboina94@gmail.com>
2020-03-12 19:42:16 +0530

    Updates

---
+++ precache-manifest.2d1e815774a9cb920a5f4def6a1830e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5bb0e59de4e7fb9b57ccdbd0497e7d",
+    "url": "/react-app2/index.html"
+  },
+  {
+    "revision": "088217aea9a78f8feba0",
+    "url": "/react-app2/static/css/main.52467fc7.chunk.css"
+  },
+  {
+    "revision": "75fe50cca0f88c7c0bbc",
+    "url": "/react-app2/static/js/2.d02fbc7c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-app2/static/js/2.d02fbc7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "088217aea9a78f8feba0",
+    "url": "/react-app2/static/js/main.9f603a67.chunk.js"
+  },
+  {
+    "revision": "6206854bf894cd1f621f",
+    "url": "/react-app2/static/js/runtime-main.c64deaef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app2/static/media/logo.5d5d9eef.svg"
+  }
+]);
